02823cd346acc2c84139e934a05d5775eccf132e
lu-git11 <jeffrey.lullen@sdgku.com>
2025-02-24 11:01:47 +0800

    story

---
+++ scripts/assignments-1.js
@@ -0,0 +1,24 @@
+//let//
+let action = "walked";
+let place = "beach";
+let item = "banana";
+
+//const//
+const name = "Jeff";
+const number = "5";
+
+
+
+
+console.log("Funny Story");
+console.log(name + " " + action + " to the " + place + ". ");
+console.log("He " + action + " into " + number + " " + item + " peels and fell in the water. ")
+
+
+action = "jumped";
+place = "pool";
+item = 'sharks';
+
+console.log(name + " " + action + " into the " + place + ". ");
+console.log("There were " + number + " " + item + " in the " + place + ". ");
+console.log(name + " " + action + " out.");
---
+++ scripts/variables.js
@@ -0,0 +1,67 @@
+console.log("Hello World!");
+console.log("My name is " + "Jeff, " + "I'm " + 30 + " years old");
+
+//This is a comment//
+// keyword + variable name = value
+//Variables( let variable name) = "value as string" string variable
+let name = "Peter";
+let lastName = "Parker"; //variable//
+//quotation is a strong//
+console.log(name);
+console.log(lastName);
+console.log("Hello " + name + " " + lastName + ".");
+
+//numerical variable
+let age = 35;
+console.log(age);
+
+let grade = 9.5;
+console.log(grade);
+
+//buulean variable(true or false)
+let isAProfessor = true;
+let isAStudent = false;
+let isAdmin = true;
+
+console.log(isAStudent);
+
+let number1 = 50;
+let number2 = 5;
+let addition = (number1 + number2)
+console.log(number1 + " plus " + number2 + " equals " + (number1+number2));
+console.log(number1 + " plus " + number2 + " equals " + addition);
+
+number1 = 7;
+number2 = 3;
+let subtraction = (number1 * number2);
+console.log(number1 + " - " + number2 + " = " + subtraction);
+let multiplication = (number1 * number2);
+console.log(number1 + " * " + number2 + " = " + multiplication);
+
+let division = (number1 / number2).toFixed(2);
+console.log(number1 + " / " + number2 + " = " + division)
+/*toFixed*/
+/*many lines of comment*/
+
+//let vs var//
+let y = 10;
+console.log(y);
+
+
+console.log(x);
+var x = 5;
+
+//let school = "SDGKU"  error
+//let school = "School2"
+
+var school = "SDGKU";
+var school = "School2";
+console.log(school);
+//dont use var, use let
+
+//constant variable
+const pi = 3.1416;
+const myName = "Jeff";
+console.log(myName);
+// value cannot be reassigned or changed. 
+// It's useful when you want to make sure the value stays the same throughout your code.
